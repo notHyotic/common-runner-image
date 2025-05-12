8df5ce900ab2014888730196c56d00c45dcbfcc4
notHyotic <hy0t1c1@gmail.com>
2025-05-12 04:07:15 -0500

    add build command

---
+++ .ops/commands/build.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"log"
+
+	"lesiw.io/cmdio/sys"
+)
+
+func (Ops) Build() {
+	var rnr = sys.Runner().WithEnv(map[string]string{
+		"PKGNAME":         "cmdio",
+	})
+	defer rnr.Close()
+
+	// Build the image
+	err := rnr.Run("docker", "build", "-t", "hy0tic/common-runner-image", ".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Log the image size
+	err = rnr.Run("sh", "-c", "docker images hy0tic/common-runner-image --format 'Image Size: {{.Size}}'")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
