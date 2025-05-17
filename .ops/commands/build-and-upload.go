e81c3fce1f484be90419b56677a449132b588a16
notHyotic <hy0t1c1@gmail.com>
2025-05-16 23:19:15 -0500

    move upload to its own function

--- .ops/commands/build-and-upload.go
+++ .ops/commands/build-and-upload.go
@@ -1,22 +1,9 @@
 package commands
 
 import (
-	"log"
 	"os"
-	"strings"
-
-	"github.com/joho/godotenv"
-	"lesiw.io/cmdio"
 	"lesiw.io/cmdio/sys"
 )
-
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No .env file found")
-		log.Println("Skipping loading .env file")
-	}
-}
 
 func (Ops) Buildandupload() {
 	var rnr = sys.Runner().WithEnv(map[string]string{
@@ -25,23 +12,8 @@ func (Ops) Buildandupload() {
 		"DOCKER_PASSWORD": os.Getenv("DOCKER_PASSWORD"),
 	})
 	defer rnr.Close()
-	var err error
 
 	Ops{}.Build()
 	
-	err = cmdio.Pipe(
-		strings.NewReader(rnr.Env("DOCKER_PASSWORD")),
-		rnr.Command("docker", "login",
-			"-u", rnr.Env("DOCKER_USERNAME"),
-			"--password-stdin",
-		),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = rnr.Run("docker", "push", "hy0tic/common-runner-image:latest")
-	if err != nil {
-		log.Fatal(err)
-	}
+	Ops{}.Upload()
 }
---
+++ .ops/commands/upload.go
@@ -0,0 +1,45 @@
+package commands
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"lesiw.io/cmdio"
+	"lesiw.io/cmdio/sys"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+		log.Println("Skipping loading .env file")
+	}
+}
+
+func (Ops) Upload() {
+	var rnr = sys.Runner().WithEnv(map[string]string{
+		"PKGNAME":         "cmdio",
+		"DOCKER_USERNAME": os.Getenv("DOCKER_USERNAME"),
+		"DOCKER_PASSWORD": os.Getenv("DOCKER_PASSWORD"),
+	})
+	defer rnr.Close()
+	var err error
+	
+	err = cmdio.Pipe(
+		strings.NewReader(rnr.Env("DOCKER_PASSWORD")),
+		rnr.Command("docker", "login",
+			"-u", rnr.Env("DOCKER_USERNAME"),
+			"--password-stdin",
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = rnr.Run("docker", "push", "hy0tic/common-runner-image:latest")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
