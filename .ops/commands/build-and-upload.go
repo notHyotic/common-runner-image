747b7ca178eac65ac06cb3fe2542565f11feee2d
notHyotic <hy0t1c1@gmail.com>
2025-05-16 22:51:25 -0500

    reuse build function

--- .ops/commands/build-and-upload.go
+++ .ops/commands/build-and-upload.go
@@ -27,16 +27,7 @@ func (Ops) Buildandupload() {
 	defer rnr.Close()
 	var err error
 
-	err = rnr.Run("docker", "build", "-t", "hy0tic/common-runner-image", ".")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = rnr.Run("docker", "images", "hy0tic/common-runner-image", 
-		"--format", "Image Size: {{.Size}}")
-	if err != nil {
-		log.Fatal(err)
-	}
+	Ops{}.Build()
 	
 	err = cmdio.Pipe(
 		strings.NewReader(rnr.Env("DOCKER_PASSWORD")),
