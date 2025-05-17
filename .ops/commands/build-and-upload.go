6e448cd8495bb44489f19658dbd8c7317433766c
notHyotic <hy0t1c1@gmail.com>
2025-05-16 23:33:31 -0500

    remove dead code

--- .ops/commands/build-and-upload.go
+++ .ops/commands/build-and-upload.go
@@ -1,19 +1,6 @@
 package commands
 
-import (
-	"os"
-	"lesiw.io/cmdio/sys"
-)
-
 func (Ops) Buildandupload() {
-	var rnr = sys.Runner().WithEnv(map[string]string{
-		"PKGNAME":         "cmdio",
-		"DOCKER_USERNAME": os.Getenv("DOCKER_USERNAME"),
-		"DOCKER_PASSWORD": os.Getenv("DOCKER_PASSWORD"),
-	})
-	defer rnr.Close()
-
 	Ops{}.Build()
-	
 	Ops{}.Upload()
 }
