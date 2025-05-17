8c292fdbe31b229247efacb27e2649bb3b9f8e67
notHyotic <hy0t1c1@gmail.com>
2025-05-16 21:08:33 -0500

    remove comment

--- .ops/commands/build.go
+++ .ops/commands/build.go
@@ -12,13 +12,11 @@ func (Ops) Build() {
 	})
 	defer rnr.Close()
 
-	// Build the image
 	err := rnr.Run("docker", "build", "-t", "hy0tic/common-runner-image", ".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Log the image size
 	err = rnr.Run("docker", "images", "hy0tic/common-runner-image", 
 		"--format", "Image Size: {{.Size}}")
 	if err != nil {
